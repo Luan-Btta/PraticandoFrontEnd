3e9c473cc90d1bc1a1b190ca4b81cb471aebc81f
Luan Batista <31670117+Luan-Btta@users.noreply.github.com>
2023-03-06 20:06:51 -0300

    Grid bootstrap finalizado

--- exc04_grid_bootstrap/index.php
+++ exc04_grid_bootstrap/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,9 +9,15 @@
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <title>Document</title>
 </head>
-<body>
-    <div class="my-box">
 
+<body>
+    <div class="my-box container">
+        <div class="row justify-content-center">
+            <div class="col-sm-3 col-lg-3 div1">DIV 1</div>
+            <div class="col-sm-8 col-lg-2 div2">DIV 2</div>
+            <div class="col-sm-1 col-lg-5 div3">DIV 3</div>
+        </div>
     </div>
 </body>
+
 </html>
